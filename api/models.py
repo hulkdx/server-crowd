7cdc15357c77aac081f93edd48f686081422d9a4
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-04-18 13:48:18 +0200

    new migration for user column

--- api/models.py
+++ api/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
+
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Proposal(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    deadline = models.DateTimeField(default=None, blank=True, null=True)
+    deadline = models.DateTimeField(default=datetime.now(), blank=True, null=True)
     description = models.CharField(max_length=500)
     # TODO: ATTACHMENT
 
