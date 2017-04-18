9c8ae2218b467db47c3dd70584d17e36f0e6e10e
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-04-18 13:31:11 +0200

    Proposals model insert a deadline and user as ForeignKey

--- api/models.py
+++ api/models.py
@@ -1,12 +1,16 @@
+from datetime import datetime
 from django.db import models
-
-from website import settings
+from django.contrib.auth.models import User
 
 
 class Proposal(models.Model):
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    title = models.CharField(max_length=40)
-    description = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    deadline = models.DateTimeField(default=None, blank=True, null=True)
+    description = models.CharField(max_length=500)
+    # TODO: ATTACHMENT
 
     def __str__(self):
         return "title: " + str(self.title)
+
+# class Category
