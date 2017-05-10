e437a1291d063cbdb5618b808295133832625c77
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-10 14:04:29 +0200

    quck fix

--- api/models.py
+++ api/models.py
@@ -1,14 +1,14 @@
-from datetime import datetime
 
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class Proposal(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    category = models.ForeignKey('Category', default=1, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', default=None, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    deadline = models.DateTimeField(default=datetime.now(), blank=True, null=True)
+    deadline = models.DateTimeField(default=timezone.now, blank=True, null=True)
     description = models.CharField(max_length=500)
     articles = models.IntegerField(default=0)
     discussions = models.IntegerField(default=0)
@@ -20,3 +20,6 @@ class Proposal(models.Model):
 
 class Category(models.Model):
     source = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "source: " + str(self.source)
