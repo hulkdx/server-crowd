910088a52a6e8a60e88ab6a3337fbad63e6baa7a
Mohammad Jafarzadeh Rezvan <hulktroll@gmail.com>
2017-05-10 13:24:25 +0200

    new models for proposals added

--- api/models.py
+++ api/models.py
@@ -6,12 +6,17 @@ from django.db import models
 
 class Proposal(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', default=1, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     deadline = models.DateTimeField(default=datetime.now(), blank=True, null=True)
     description = models.CharField(max_length=500)
+    articles = models.IntegerField(default=0)
+    discussions = models.IntegerField(default=0)
     # TODO: ATTACHMENT
 
     def __str__(self):
         return "title: " + str(self.title)
 
-# class Category
+
+class Category(models.Model):
+    source = models.CharField(max_length=200)
